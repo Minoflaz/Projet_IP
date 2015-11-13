978cee95ffc9232e2457a0c837205ce85ab6739e
Elyos <elyes.labassi@gmail.com>
2015-11-13 18:40:57 +0100

    ajout partie fonc. JAVA

---
+++ adressageIP_JAVA/src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AfficheBits application = new AfficheBits();
+        application.setDefaultCloseOperation(
+                JFrame.EXIT_ON_CLOSE
+        );
+    }
+}
