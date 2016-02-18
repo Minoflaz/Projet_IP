ea44fb266b53ba1227cf14584b6e669813a39aa3
minoflaz <minoflaz95@hotmail.fr>
2016-02-18 18:19:35 +0100

    chapitres renfill

--- Symfony_PHP_project/src/IPBundle/Controller/DefaultController.php
+++ Symfony_PHP_project/src/IPBundle/Controller/DefaultController.php
@@ -34,6 +34,14 @@ class DefaultController extends Controller
 
         $cours = $this->getDoctrine()->getRepository('IPBundle:Cours')->findAll();
 
+        $chapitres = $this->getDoctrine()->getRepository('IPBundle:Chapitre')->findAll();
+
+        foreach($chapitres as $chapitre) {
+
+            $chapitre->setText("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?");
+
+        }
+
         $chapitres = $this->getDoctrine()->getRepository('IPBundle:Chapitre')->findBy([], ['id' => 'DESC']);
 
 
